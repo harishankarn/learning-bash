f0d9dc0048ab9957787d7e9491897412803f201d
BtMn <btmn@BtMns-MacBook-Pro.local>
2023-07-19 12:30:34 +0530

    improvments

--- auto_commit.sh
+++ auto_commit.sh
@@ -5,6 +5,17 @@
 
 git add .
 echo "enter message : " 
+
+# read stores the user input into 
+# the variable "message"
+
 read message
+
+# To print out the text in the variable,
+# we put '$' in front of the variable.
+
+#generally we us it inside the quotes
+# of ' echo" " '
+
 git commit -m $message 
 git push
